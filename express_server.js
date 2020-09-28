c4c00e23f996eae3b580ea7868b94bcc3bc455e5
jilliankmartin <jillmartin@live.com>
2020-09-28 21:40:37 +0000

    added new urls template

--- express_server.js
+++ express_server.js
@@ -21,6 +21,11 @@ app.set("view engine", "ejs");
    res.send("<html><body>Hello <b>World</b></body></html>\n");
  });
 
+ app.get("/urls", (req, res) => {
+   const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
  app.listen(PORT, () => {
    console.log(`Example app listening on port ${PORT}!`);
  });
