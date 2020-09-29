f0a2100b9fe45f265bc2c5a54b20f614f3f7bfbe
jilliankmartin <jillmartin@live.com>
2020-09-29 01:23:57 +0000

    added post request handler

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
+var randomString = require("randomstring");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -8,6 +11,10 @@ app.set("view engine", "ejs");
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
  };
+
+ function generateRandomString() {
+  return randomString.generate(6);
+ }
 
  app.get("/", (req, res) => {
    res.send("Hello!");
@@ -25,6 +32,15 @@ app.set("view engine", "ejs");
    const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+})
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
